dda1d2f02dbf9e91b347b10cb7d73c24f80c46c3
Charlie Showley <cgshowley@gmail.com>
2025-08-05 15:44:45 -0700

    successful first transcription

--- record_audio_keypress.py
+++ record_audio_keypress.py
@@ -3,6 +3,10 @@ import soundfile as sf
 import numpy as np
 import time
 from pynput import keyboard
+import whisperx
+import gc
+import torch
+
 
 def record_audio(filename, fs=44100):
     """
@@ -55,7 +59,39 @@ def record_audio(filename, fs=44100):
     else:
         print("No valid key press detected.")
 
-def transcribe_audio(output_filename):
+def transcribe_audio(audio_file):
+    # Set device
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    # elif torch.backends.mps.is_available():
+    #     device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+
+    batch_size = 16 # reduce if low on GPU mem
+    compute_type = "int8" # "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
+    print('loading model')
+    # 1. Transcribe with original whisper (batched)
+    # model = whisperx.load_model("large-v2", device=device, compute_type=compute_type)
+    model = whisperx.load_model("large-v2", 'cpu', compute_type='int8')
+
+    # save model to local path (optional)
+    # model_dir = "/path/"
+    # model = whisperx.load_model("large-v2", device, compute_type=compute_type, download_root=model_dir)
+    print('loading audio')
+    audio = whisperx.load_audio(audio_file)
+    result = model.transcribe(audio, batch_size=batch_size, language='en')
+    print('printing transcription\n-----------')
+    print(result["segments"][0]['text']) # before alignment
+
+    # delete model if low on GPU resources
+    # import gc; import torch; gc.collect(); torch.cuda.empty_cache(); del model
+
+    # 2. Align whisper output
+    # model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+    # result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+
+    # print(result["segments"]) # after alignment
     return
 
 def format_request():
@@ -70,7 +106,7 @@ if __name__ == "__main__":
             timestamp = int(time.time())
             output_filename = f"output_{timestamp}.wav"
             record_audio(output_filename)
-            # transcribe_audio(output_filename) # load recording and transcribe to text file
+            transcribe_audio(output_filename) # load recording and transcribe to text file
             # format_request() # append text file to llm conversation history, send api request
             # speak_reply() # convert request response to speech
     except KeyboardInterrupt:
