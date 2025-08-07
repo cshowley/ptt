0ee6128a41f1599aedbeebb68f1339f23fc194fa
Charlie Showley <cgshowley@gmail.com>
2025-08-07 10:24:56 -0700

    fix transcription dropouts, remove comments

--- record_audio_keypress.py
+++ record_audio_keypress.py
@@ -4,13 +4,14 @@ import numpy as np
 import time
 from pynput import keyboard
 import whisperx
-import gc
 import torch
 from api_call import *
 import subprocess
 from dotenv import load_dotenv; load_dotenv()
-import json
+import re
 
+# load speech-to-text model
+model = whisperx.load_model("large-v2", 'cpu', compute_type='int8')
 
 def record_audio(filename, fs=44100):
     """
@@ -63,40 +64,21 @@ def record_audio(filename, fs=44100):
     else:
         print("No valid key press detected.")
 
-model = whisperx.load_model("large-v2", 'cpu', compute_type='int8')
 def transcribe_audio(audio_file):
     # Set device
     if torch.cuda.is_available():
         device = torch.device("cuda")
-    # elif torch.backends.mps.is_available():
-    #     device = torch.device("mps")
     else:
         device = torch.device("cpu")
 
     batch_size = 16 # reduce if low on GPU mem
-    compute_type = "int8" # "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
-    print('loading model')
-    # 1. Transcribe with original whisper (batched)
-    # model = whisperx.load_model("large-v2", device=device, compute_type=compute_type)
-    # model = whisperx.load_model("large-v2", 'cpu', compute_type='int8')
 
-    # save model to local path (optional)
-    # model_dir = "/path/"
-    # model = whisperx.load_model("large-v2", device, compute_type=compute_type, download_root=model_dir)
     print('loading audio')
     audio = whisperx.load_audio(audio_file)
     result = model.transcribe(audio, batch_size=batch_size, language='en')
     print('printing transcription\n-----------')
     print(result["segments"][0]['text']) # before alignment
 
-    # delete model if low on GPU resources
-    # import gc; import torch; gc.collect(); torch.cuda.empty_cache(); del model
-
-    # 2. Align whisper output
-    # model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
-    # result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
-
-    # print(result["segments"]) # after alignment
     # remove audio file
     os.remove(audio_file)
     return result["segments"][0]['text'] 
@@ -147,7 +129,9 @@ def speak_reply(text_response):
     
     chunks = chunk_text(text_response)
     for chunk in chunks:
-        print(len(chunk))
+        if len(chunk) == 0:
+            continue
+        print(f'Audio chunk: {chunk}')
         data = {
             "model": "tts-kokoro",
             "response_format": "mp3",
